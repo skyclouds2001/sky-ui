e4968e931d6d37440e8b652c974774d8977be552
skyclouds2001 <skyclouds2001@163.com>
2024-01-02 21:19:59 +0800

    build: use lit & small update

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
-import path from 'node:path'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(process.cwd(), 'src'),
-    },
-  },
   plugins: [
     checker({
+      overlay: false,
       eslint: {
         lintCommand: 'eslint "./../src/**/*.{js,jsx,ts,tsx}"',
       },
