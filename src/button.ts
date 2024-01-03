8e8d8a89774a372927bf5611f631dc516ffb73fc
skyclouds2001 <skyclouds2001@163.com>
2024-01-03 16:01:51 +0800

    feat: add button - disabled round circle support

--- src/button.ts
+++ src/button.ts
@@ -1,14 +1,31 @@
-import { LitElement, html, css } from 'lit'
+import { LitElement, css, html, nothing } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
 @customElement('sky-button')
 export class SkyButton extends LitElement {
-  @property()
+  @property({
+    type: String,
+  })
   type?: 'primary' | 'success' | 'warning' | 'danger' | 'info'
+
+  @property({
+    type: Boolean,
+  })
+  round: boolean = false
+
+  @property({
+    type: Boolean,
+  })
+  circle: boolean = false
+
+  @property({
+    type: Boolean,
+  })
+  disabled: boolean = false
 
   render() {
     return html`
-      <button class="sky-button ${this.type != null ? `sky-button-${this.type}` : ''}">
+      <button class="sky-button ${this.type != null ? `sky-button-${this.type}` : ''} ${this.round ? 'sky-button-round' : ''} ${this.circle ? 'sky-button-circle' : ''} ${this.disabled ? 'sky-button-disabled' : ''}" ?disabled=${this.disabled} aria-disabled=${this.disabled || nothing}>
         <slot></slot>
       </button>
     `
@@ -26,6 +43,9 @@ export class SkyButton extends LitElement {
       --sky-button-hover-bg-color: #ecf5ff;
       --sky-button-hover-border-color: #c6e2ff;
       --sky-button-outline-color: #a0cfff;
+      --sky-button-disabled-text-color: #a8abb2;
+      --sky-button-disabled-bg-color: #ffffff;
+      --sky-button-disabled-border-color: #e4e7ed;
     }
 
     .sky-button {
@@ -77,6 +97,9 @@ export class SkyButton extends LitElement {
       --sky-button-hover-bg-color: #79bbff;
       --sky-button-hover-border-color: #79bbff;
       --sky-button-outline-color: #a0cfff;
+      --sky-button-disabled-text-color: #ffffff;
+      --sky-button-disabled-bg-color: #a0cfff;
+      --sky-button-disabled-border-color: #a0cfff;
     }
 
     .sky-button-success {
@@ -90,6 +113,9 @@ export class SkyButton extends LitElement {
       --sky-button-hover-bg-color: #95d475;
       --sky-button-hover-border-color: #95d475;
       --sky-button-outline-color: #b3e19d;
+      --sky-button-disabled-text-color: #ffffff;
+      --sky-button-disabled-bg-color: #b3e19d;
+      --sky-button-disabled-border-color: #b3e19d;
     }
 
     .sky-button-warning {
@@ -103,6 +129,9 @@ export class SkyButton extends LitElement {
       --sky-button-hover-bg-color: #eebe77;
       --sky-button-hover-border-color: #eebe77;
       --sky-button-outline-color: #f3d19e;
+      --sky-button-disabled-text-color: #ffffff;
+      --sky-button-disabled-bg-color: #f3d19e;
+      --sky-button-disabled-border-color: #f3d19e;
     }
 
     .sky-button-danger {
@@ -116,6 +145,9 @@ export class SkyButton extends LitElement {
       --sky-button-hover-bg-color: #f89898;
       --sky-button-hover-border-color: #f89898;
       --sky-button-outline-color: #fab6b6;
+      --sky-button-disabled-text-color: #ffffff;
+      --sky-button-disabled-bg-color: #fab6b6;
+      --sky-button-disabled-border-color: #fab6b6;
     }
 
     .sky-button-info {
@@ -129,6 +161,27 @@ export class SkyButton extends LitElement {
       --sky-button-hover-bg-color: #b1b3b8;
       --sky-button-hover-border-color: #b1b3b8;
       --sky-button-outline-color: #c8c9cc;
+      --sky-button-disabled-text-color: #ffffff;
+      --sky-button-disabled-bg-color: #c8c9cc;
+      --sky-button-disabled-border-color: #c8c9cc;
+    }
+
+    .sky-button.sky-button-disabled,
+    .sky-button.sky-button-disabled:hover,
+    .sky-button.sky-button-disabled:focus {
+      cursor: not-allowed;
+      color: var(--sky-button-disabled-text-color);
+      background-color: var(--sky-button-disabled-bg-color);
+      border-color: var(--sky-button-disabled-border-color);
+    }
+
+    .sky-button.sky-button-round {
+      border-radius: 1em;
+    }
+
+    .sky-button.sky-button-circle {
+      border-radius: 50%;
+      width: 2em;
     }
   `
 }
