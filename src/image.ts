e6c57d226994c3a74b07515022bda4bfe8948eb0
skyclouds2001 <skyclouds2001@163.com>
2024-01-07 18:47:15 +0800

    feat: add image decoding & fetchpriority & fit option

--- src/image.ts
+++ src/image.ts
@@ -1,5 +1,6 @@
 import { LitElement, css, html, nothing } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import { styleMap } from 'lit/directives/style-map.js'
 
 @customElement('sky-image')
 export class SkyImage extends LitElement {
@@ -28,10 +29,37 @@ export class SkyImage extends LitElement {
   })
   referrerpolicy?: 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'
 
+  @property({
+    type: String,
+  })
+  decoding?: 'sync' | 'async' | 'auto'
+
+  @property({
+    type: String,
+  })
+  fetchpriority?: 'high' | 'low' | 'auto'
+
+  @property({
+    type: String,
+  })
+  fit?: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down'
+
   protected render() {
     return html`
       <div class="sky-image">
-        <img class="sky-image--inner" src=${this.src} alt=${(this.alt ?? nothing) as string} crossorigin=${(this.crossorigin ?? nothing) as 'anonymous' | 'use-credentials'} loading=${(this.loading ?? nothing) as 'eager' | 'lazy'} referrerpolicy=${(this.referrerpolicy ?? nothing) as 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'} />
+        <img
+          class="sky-image--inner"
+          style=${styleMap({
+            'object-fit': this.fit,
+          })}
+          src=${this.src}
+          alt=${(this.alt ?? nothing) as string}
+          crossorigin=${(this.crossorigin ?? nothing) as 'anonymous' | 'use-credentials'}
+          loading=${(this.loading ?? nothing) as 'eager' | 'lazy'}
+          referrerpolicy=${(this.referrerpolicy ?? nothing) as 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'}
+          decoding=${(this.decoding ?? nothing) as 'sync' | 'async' | 'auto'}
+          fetchpriority=${this.fetchpriority ?? nothing}
+        />
       </div>
     `
   }
