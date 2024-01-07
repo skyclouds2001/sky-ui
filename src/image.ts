a7a0e26a20be699167f1fda0a1a7a067d4049a86
skyclouds2001 <skyclouds2001@163.com>
2024-01-07 22:00:14 +0800

    feat: add image width&height prop placeholder&error slot

--- src/image.ts
+++ src/image.ts
@@ -1,6 +1,7 @@
 import { LitElement, css, html, nothing } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { customElement, property, state } from 'lit/decorators.js'
 import { styleMap } from 'lit/directives/style-map.js'
+import { when } from 'lit/directives/when.js'
 
 @customElement('sky-image')
 export class SkyImage extends LitElement {
@@ -8,6 +9,16 @@ export class SkyImage extends LitElement {
     type: String,
   })
   src: string = ''
+
+  @property({
+    type: Number,
+  })
+  width?: number
+
+  @property({
+    type: Number,
+  })
+  height?: number
 
   @property({
     type: String,
@@ -44,22 +55,60 @@ export class SkyImage extends LitElement {
   })
   fit?: 'fill' | 'contain' | 'cover' | 'none' | 'scale-down'
 
+  @state()
+  private isLoading = true
+
+  @state()
+  private isError = false
+
+  private readonly onLoad = () => {
+    this.isLoading = false
+    this.isError = false
+  }
+
+  private readonly onError = () => {
+    this.isLoading = false
+    this.isError = true
+  }
+
   protected render() {
     return html`
       <div class="sky-image">
         <img
+          ?hidden=${this.isError}
           class="sky-image--inner"
           style=${styleMap({
             'object-fit': this.fit,
+            opacity: this.isLoading ? 0 : 1,
           })}
           src=${this.src}
+          width=${(this.width ?? nothing) as number}
+          height=${(this.height ?? nothing) as number}
           alt=${(this.alt ?? nothing) as string}
           crossorigin=${(this.crossorigin ?? nothing) as 'anonymous' | 'use-credentials'}
           loading=${(this.loading ?? nothing) as 'eager' | 'lazy'}
           referrerpolicy=${(this.referrerpolicy ?? nothing) as 'no-referrer' | 'no-referrer-when-downgrade' | 'origin' | 'origin-when-cross-origin' | 'same-origin' | 'strict-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'}
           decoding=${(this.decoding ?? nothing) as 'sync' | 'async' | 'auto'}
           fetchpriority=${this.fetchpriority ?? nothing}
+          @load=${this.onLoad}
+          @error=${this.onError}
         />
+        ${when(
+          this.isLoading,
+          () => html`
+            <div class="sky-image--placeholder">
+              <slot name="placeholder">loading</slot>
+            </div>
+          `
+        )}
+        ${when(
+          this.isError,
+          () => html`
+            <div class="sky-image--error" ?hidden=${!this.isError}>
+              <slot name="error">error</slot>
+            </div>
+          `
+        )}
       </div>
     `
   }
@@ -71,10 +120,22 @@ export class SkyImage extends LitElement {
       overflow: hidden;
     }
 
-    .sky-image--inner {
+    .sky-image--inner,
+    .sky-image--placeholder,
+    .sky-image--error {
       width: 100%;
       height: 100%;
+    }
+
+    .sky-image--placeholder,
+    .sky-image--error {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       vertical-align: middle;
+      background-color: #f5f7fa;
+      color: #a8abb2;
+      font-size: 14px;
     }
   `
 }
