ce9e4e02986d37aaf09ca40eeefce50ad241314e
skyclouds2001 <skyclouds2001@163.com>
2024-01-07 22:41:31 +0800

    feat: add icon size&color prop

--- src/icon.ts
+++ src/icon.ts
@@ -1,11 +1,28 @@
 import { LitElement, css, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { customElement, property } from 'lit/decorators.js'
+import { styleMap } from 'lit/directives/style-map.js'
 
-@customElement('sky-divider')
+@customElement('sky-icon')
 export class SkyIcon extends LitElement {
+  @property({
+    type: Number,
+  })
+  size?: number
+
+  @property({
+    type: String,
+  })
+  color?: string
+
   protected render() {
     return html`
-      <i class="sky-icon">
+      <i
+        class="sky-icon"
+        style=${styleMap({
+          'font-size': this.size != null ? `${this.size}px` : null,
+          color: this.color,
+        })}
+      >
         <slot></slot>
       </i>
     `
